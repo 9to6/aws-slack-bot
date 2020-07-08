350caf5e3bcec71d68b17b3e39eb679204c2ba0c
Teukgeon Kwon <teukgeon.kwon@nielsen.com>
2020-07-08 14:52:36 +0900

    .

--- aws/aws.go
+++ aws/aws.go
@@ -2,7 +2,10 @@ package aws
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/9to6/aws-slack-bot/config"
 )
@@ -38,4 +41,20 @@ func newSession(region, accessKey, secretAccessKey string) (*session.Session, er
 	}
 
 	return sess, nil
+}
+
+func NewEc2WithRole(role string, cfg *config.Config) (*ec2.EC2, error) {
+	sess := session.Must(session.NewSession())
+
+	// Create the credentials from AssumeRoleProvider to assume the role
+	// referenced by the "myRoleARN" ARN.
+	creds := stscreds.NewCredentials(sess, role)
+
+	// Create service client value configured for credentials
+	// from assumed role.
+	conf := &aws.Config{
+		Credentials: creds,
+		Region: aws.String(cfg.Aws[0].Region),
+	}
+	return ec2.New(sess, conf), nil
 }
